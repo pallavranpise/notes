91b3ae1f25dd70285604081ae61d8cc7b8e5e256
pallavranpise <pallav.ranpise@gmail.com>
2024-12-05 00:11:37 +0530

    Initial Commit

---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "notes",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    await import("./infra/storage");
+    await import("./infra/api");
+  },
+});
